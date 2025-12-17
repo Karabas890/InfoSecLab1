36e731873d66bd3a08ab966ec542a5d35cd661a8
Karabas890 <ivan.mitichev@mail.ru>
2025-12-17 09:44:00 +0300

    final version

--- src/main/java/itmo/ru/infosec/configuration/SecurityConfig.java
+++ src/main/java/itmo/ru/infosec/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package itmo.ru.infosec.configuration;
 
 import itmo.ru.infosec.filter.AuthorizationFilter;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,7 +10,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 public class SecurityConfig {
-
+    @SuppressFBWarnings(value = "SPRING_CSRF_PROTECTION_DISABLED",
+            justification = "JWT API does not use cookies, CSRF protection not needed")
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthorizationFilter authFilter) throws Exception {
         http
